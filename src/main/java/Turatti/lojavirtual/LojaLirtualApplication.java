d22cfd09265ca437b9131e6c5e297e59b0cb83e2
Usuario <Usuario@MASTER>
2022-08-09 17:47:51 -0400

    Commit inicial projeto Loja Virtual

---
+++ src/main/java/Turatti/lojavirtual/LojaLirtualApplication.java
@@ -0,0 +1,13 @@
+package Turatti.lojavirtual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaLirtualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaLirtualApplication.class, args);
+	}
+
+}
